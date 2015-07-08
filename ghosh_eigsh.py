1467ec3aac1ddd91f1904c6ec8ea9c5af9dac1d4
sheyma <sheymaba@gmail.com>
2015-07-08 09:49:08 +0200

    algebraic comments for diffusion map

--- ghosh_eigsh.py
+++ ghosh_eigsh.py
@@ -5,38 +5,38 @@ from sklearn.utils.arpack import eigsh
 from pylab import *
 import numpy as np
 
+# diffusion map corrdiantes : DoFiedler
 # where conn is a symmetrical connectivity matrix:
 def DoFiedler(conn):
     # prep for embedding
+    # K : matrix of similarities
     K = (conn + 1) / 2.
-    # axis=1 meaning operating over rows
+    # axis=1 meaning operating over rows, "row sum's of K"
     v = np.sqrt(np.sum(K, axis=1))
-    A = K/(v[:, None] * v[None, :])
+    # make a random walk on data, D is diagonal matrix
+    D = v[:, None] * v[None, :]
+    # row-normalization of K gives transition matrix A => A = D^-1 * K
+    A = K/D
     del K
     A = np.squeeze(A * [A > 0])
-    print "ASze" , np.shape(A)
     # diffusion embedding (original value of n_components_embedding=5)
-    n_components_embedding = matrix_rank(A, tol=None)/2
-    print "n_comp" , n_components_embedding
+    n_components_embedding = matrix_rank(A) / 2
     # k must be < rank(A)
     lambdas, vectors = eigsh(A, k=n_components_embedding)
     del A
-    print  "lambda_before", lambdas
     # sorting eigenvalues and -vectors in descending order
     lambdas = lambdas[::-1]
-    print "lambda_aft", lambdas
     vectors = vectors[:, ::-1]
     psi = vectors/vectors[:, 0][:, None]
-    # begin from second largest eigenvalue and ?
+    # begin from second largest eigenvalue and
     lambdas = lambdas[1:] / (1 - lambdas[1:])
     embedding = psi[:, 1:(n_components_embedding + 1)] * lambdas[:n_components_embedding][None, :]
-    #print "embedding" , embedding
     return embedding
 
-def symmetrize(matrix):
-    return matrix + matrix.T - np.diag(matrix.diagonal())
+# symmetrizing a matrix
+def symmetry(a):
+    return a + a.T - np.diag(a.diagonal())
 
-
-A = np.random.randn(12,12)
-A = symmetrize(A)
-B = DoFiedler(A)
+tmp = np.random.randn(12, 12)
+tmp = symmetry(tmp)
+B = DoFiedler(tmp)

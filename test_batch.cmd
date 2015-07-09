b944e4c528d5318b2bbe56cce164d1443a1d9c5a
sheyma <sheymaba@gmail.com>
2015-07-09 12:02:20 +0200

    line-to-line copy from hydra hp

--- test_batch.cmd
+++ test_batch.cmd
@@ -1,21 +1,21 @@
-#----sample batch script--------
-#@ shell = /bin/bash
-#@ jobname = hello_hydra
-#@ initialdir = /u/sbayrak/devel/eigen_decomp
-#@ input = $(initialdir)/random_mtx.dat
-#@ error = job.err.$(jobid)
-#@ output = job.out.$(jobid)
-#@ job_type = parallel
-#@ node_usage = not_shared
-#@ node = 32
-#@ resources = ConsumableCpus(1)
-#@ wall_clock_limit = 00:50:00
-#@ notification = complete
-#@ notify_user = $(user)@rzg.mpg.de
-#@ queue
+# @ shell=/bin/bash
+#
+# Sample script for LoadLeveler
+#
+# @ error = job.err.$(jobid)
+# @ output = job.out.$(jobid)
+# @ job_type = parallel
+# @ node_usage= not_shared
+# @ node = 32
+# @ tasks_per_node = 16
+# @ resources = ConsumableCpus(1)
+# @ network.MPI = sn_all,not_shared,us
+# @ wall_clock_limit = 24:00:00
+# @ notification = complete
+# @ notify_user = $(user)@rzg.mpg.de
+# @ queue
 
-# runt the program
+# run the program
 
 cd /ptmp/${USER}/
 poe /u/${USER}/myprog > prog.out
-

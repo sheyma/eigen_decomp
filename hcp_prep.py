db4b734dab048c437d0cb8736b9afa94d8b8f6f6
sheyma <sheymaba@gmail.com>
2015-07-24 17:35:59 +0200

    read-in data from MPI, and save-out

--- hcp_prep.py
+++ hcp_prep.py
@@ -2,79 +2,68 @@
 # pip install git+https://github.com/satra/nibabel.git@enh/cifti2
 
 from glob import glob
-import os, numpy as np
+import os
+import numpy as np
 import nibabel as nb
 
-data_dir = '/ptmp/mdani/hcp' #'/a/documents/connectome/_all'
-# Set as local output directory:
-results_dir = './'
+# set as a local input directory (MPI)
+data_path = '/a/documents/connectome/_all'
+# set a list for the subject ID's
+subject_list = ['100307', '100408', '101006', '101107', '101309']
+# set as local output directory:
+out_path = '/home/raid/bayrak/devel/eigen_decomp'
 
-subject_list = ['100307' '100408' '101006' '101107' '101309']
+# data_path = '/ptmp/mdani/hcp'
+# template = ('%s/rfMRI_REST?_??_Atlas_hp2000_clean.dtseries.nii' % subject)
+# img = nb.load('/ptmp/mdani/hcp/100307/rfMRI_REST1_LR_Atlas_hp2000_clean.dtseries.nii')
 
-def createCorrMatrix(subject):
-    # For local institute:
-    #template = ('%s/MNINonLinear/Results/rfMRI_REST?_??/rfMRI_REST?_??_Atlas_hp2000_clean.dtseries.nii' % subject)
-    template = ('%s/rfMRI_REST?_??_Atlas_hp2000_clean.dtseries.nii' % subject)
-    files = [val for val in sorted(glob(os.path.join(data_dir, template)))]
+def correlation_matrix(subject):
+    template = ('%s/MNINonLinear/Results/rfMRI_REST?_??/rfMRI_REST?_??_Atlas_hp2000_clean.dtseries.nii' % subject)
+    files = [val for val in sorted(glob(os.path.join(data_path, template)))]
     filename = files[:2]
-    print filename
 
     # read in data and create correlation matrix:
-    # for left hemisphere; 'range(0,nsamples)' for all
-    # data_range = range(0,32492) 
+    # for left hemisphere; 'range(0,nsamples)' for all ??
+    # data_range = range(0,32492) ??
 
-    input_timeseries = []
-    for x in xrange(0,4):
+    tmp_t_series = []
+    for x in xrange(0, 4):
+        # why is it not filename[x]?
         img = nb.load(filename[0])
-        ntimepoints, nsamples = img.data.shape
+        # ntimepoints, nsamples = img.data.shape
 
         # the following should be a concatenation of all 4 scans from each subject:
         # brainModels[2] will include both left and right hemispheres
         # for only left hemisphere: brainModels[1]
-        single_run_timeseries = img.data[:, :img.header.matrix.mims[1].brainModels[2].indexOffset].T
-        # old: img.get_data()[:,:,:,:,:,data_range].squeeze().T
-        input_timeseries.extend(((single_run_timeseries - single_run_timeseries.mean(axis=0)) / single_run_timeseries.std(axis=0)).T)
-        del img
-        del single_run_timeseries
 
-    # create correlation matrix and shift values between [0,1]:
-    K = (np.corrcoef(np.array(input_timeseries).T) + 1) / 2.
-    del input_timeseries
-    print K
+        header = img.header.matrix.mims[1].brainModels[2].indexOffset
+        single_t_series = img.data[:, :header].T
+
+        mean_series = single_t_series.mean(axis=0)
+        std_series = single_t_series.std(axis=0)
+
+        tmp_t_series.extend(((single_t_series - mean_series) / std_series).T)
+
+        del img
+        del single_t_series
+
+    # K : matrix of similarities / Kernel matrix / Gram matrix
+    K = (np.corrcoef(np.array(tmp_t_series).T) + 1) / 2.
+    del tmp_t_series
+
     return K
 
-A = createCorrMatrix('100307')
 
-# import nibabel as nb
-#
-# img = nb.load('/ptmp/mdani/hcp/100307/rfMRI_REST1_LR_Atlas_hp2000_clean.dtseries.nii')
-#
-# img
-#
-# img.data
-#
-# img.data.shape
-#
-# single_run_timeseries = img.data[:, :img.header.matrix.mims[1].brainModels[2].indexOffset].T
-#
-# single_run_timeseries.shape
-#
-# single_run_timeseries = img.data[:, :100].T
-#
-# single_run_timeseries.shape
-#
-# (100, 1200)
-#
-# import numpy as np
-#
-# K = (np.corrcoef(np.array(single_run_timeseries).T) + 1) / 2.
-#
-# K.shape
-#
-# (1200, 1200)
-#
-# K = (np.corrcoef(np.array(single_run_timeseries)) + 1) / 2.
-#
-# K.shape
-#
-# (100, 100)
+def save_output(subject, matrix):
+    out_dir = os.path.join(out_path, subject)
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    filename = 'bla'
+    out_file = os.path.join(out_dir, filename)
+
+    np.savetxt(out_file, matrix, fmt='%.3e', delimiter='\t', newline='\n')
+    return out_file
+
+
+K = correlation_matrix(subject_list[0])
+save_output(subject_list[0], K)

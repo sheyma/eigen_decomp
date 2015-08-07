002f808e1ef4875cb55f81494f30a7221b6bf36f
sheyma <sheymaba@gmail.com>
2015-08-07 14:49:36 +0200

    add hcp job script for hydra

---
+++ hcp_job.cmd
@@ -0,0 +1,28 @@
+# @ shell=/bin/bash
+#
+# Sample script for LoadLeveler
+#
+# @ error = job.$(jobid).err
+# @ output = job.$(jobid).out
+# @ job_type = parallel
+# @ node_usage= not_shared
+# @ node = 1
+# @ tasks_per_node = 1
+# @ resources = ConsumableCpus(4)
+# @ node_resources = ConsumableMemory(100gb)
+# @ network.MPI = sn_all,not_shared,us
+# @ wall_clock_limit = 24:00:00
+# @ notification = complete
+# @ notify_user = $(user)@rzg.mpg.de
+# @ queue
+
+echo "#### begin env"
+hostname -f
+free
+ulimit -a
+env | sort
+echo "#### end env"
+
+# here we go
+
+/usr/bin/time -v python /u/${USER}/devel/eigen_decomp/hcp_prep.py

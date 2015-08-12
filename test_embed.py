2e5c727abbbad611cfc3a09b55a475c47681a72a
sheyma <sheymaba@gmail.com>
2015-08-12 17:04:59 +0200

    .csv read-in to test

--- test_embed.py
+++ test_embed.py
@@ -1,49 +1,38 @@
 import numpy as np
 import sys
+import os
+import scipy
+import sklearn
+print "aaaa scipy", scipy.__version__
+print "bbbb numpy", np.__version__
+print "ccccc sklearn", sklearn.__version__
 
 satra_path = sys.path.append('/home/raid/bayrak/devel/mapalign/mapalign')
-
 import embed
 
-tmp_mtx = np.random.randint(1, 10, (8000, 8000)) / float(100)
-tmp_mtx += tmp_mtx.T - np.diag(tmp_mtx.diagonal())
+subject = sys.argv[1]
+# e.g. /ptmp/sbayrak/hcp_prep_out/100307...
 
-print tmp_mtx.shape
-#np.savetxt('test_matrix.csv', tmp_mtx, fmt='%5.5e', delimiter='\t', newline='\n')
+out_path = '/home/raid/bayrak/devel/embed_out '
+# set as local output directory (HYDRA)
 
-
-#csv = np.loadtxt('test_matrix.csv')
-
-#print csv
-
-embedding, result = embed.compute_diffusion_map(tmp_mtx, alpha=0, n_components=5, diffusion_time=0)
+L = np.loadtxt(subject)
 
 
+def save_output(subject, embed_matrix):
+    out_dir = os.path.join(out_path)
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    filename = os.path.basename(subject[:-4]) + '_embed_out.csv'
+    print filename
+    out_file = os.path.join(out_dir, filename)
+    # %.e = Floating point exponential format (lowercase)
+    np.savetxt(out_file, embed_matrix, fmt='%5.5e', delimiter='\t', newline='\n')
+    return out_file
 
-# data_path = '/a/documents/connectome/_all'
-# subject = '../q5/965771/MNINonLinear/T1w.nii.gz'
-#
-# example_filename = os.path.join(data_path, subject)
-# img = nib.load(example_filename)
-#
-# print "class of data: ", type(img)
-# print "shape of data: ", img.shape
-# print "type of data: ", img.get_data_dtype()
-#
-# img_new = img.get_data()
-#
-# def show_slices(slices):
-#     fig, axes = pl.subplots(1, len(slices))
-#     for i, slice in enumerate(slices):
-#         print slice.shape
-#         axes[i].imshow(slice.T, cmap='gray', origin="lower")
-#
-# # sclices are numpy arrays
-# slice_0 = img_new[:, 10, :]
-# slice_1 = img_new[:, 50, :]
-# slice_2 = img_new[:, 70, :]
-#
-# show_slices([slice_0, slice_1, slice_2])
-# pl.suptitle("bla")
-# #pl.show()
+embedding, result = embed.compute_diffusion_map(L, alpha=0, n_components=3, diffusion_time=0)
+
+save_output(subject, embedding)
+
+print result['lambdas']
 

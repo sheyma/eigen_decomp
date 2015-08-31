c1b73b5baca66f5b26c836c403796d7b934bb777
Şeyma Bayrak <sheymaba@gmail.com>
2015-08-31 16:33:27 +0200

    batch modifications

--- embed_job.cmd
+++ embed_job.cmd
@@ -2,8 +2,8 @@
 #
 # Sample script for LoadLeveler
 #
-# @ error = job.$(jobid).err
-# @ output = job.$(jobid).out
+# @ error = /ptmp/sbayrak/embed_job_stat/job.$(jobid).err
+# @ output = /ptmp/sbayrak/embed_job_stat/job.$(jobid).out
 # @ job_type = parallel
 # @ node_usage= not_shared
 # @ node = 1
@@ -11,7 +11,7 @@
 # @ resources = ConsumableCpus(4)
 # @ node_resources = ConsumableMemory(55gb)
 # @ network.MPI = sn_all,not_shared,us
-# @ wall_clock_limit = 24:00:00
+# @ wall_clock_limit = 00:50:00
 # @ notification = complete
 # @ notify_user = $(user)@rzg.mpg.de
 # @ queue
@@ -25,5 +25,5 @@ echo "#### end env"
 
 # here we go
 
-/usr/bin/time -v python /u/${USER}/devel/eigen_decomp/test_embed.py /ptmp/sbayrak/hcp_prep_out/100307_hcp_prep_out.csv
+/usr/bin/time -v python /u/${USER}/devel/eigen_decomp/test_embed.py /ptmp/${USER}/hcp_prep_out/%SUBJ_ID%
 

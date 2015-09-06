2b55bdd2151996a24fbd400fa725eccd1b710238
Şeyma Bayrak <sheymaba@gmail.com>
2015-09-06 15:07:39 +0200

    test_embed.py: load original .nii files and calculate correlation matrix
    Loading such huge (hcp_prep'ed) matrix from file was too slow
    and I/O invasive.
    
    Example:
    Such a matrix (n=60000) is about 40G on disk (uncompressed CSV). It takes
    about 30 minutes to load it again. Loading the small original .nii files
    and calculate the correlation matrix will take only 3-4 minutes. The
    downside is that numpy.corrcoef() needs about 85GB memory for this
    ... 3 times more than theoretical possible minimum (8 x n^2).

--- test_embed.py
+++ test_embed.py
@@ -4,6 +4,9 @@ import os
 import scipy
 import sklearn
 import csv
+from glob import glob
+import nibabel as nb
+
 print "aaaa scipy", scipy.__version__
 print "bbbb numpy", np.__version__
 print "ccccc sklearn", sklearn.__version__
@@ -11,8 +14,9 @@ print "ccccc sklearn", sklearn.__version__
 satra_path = sys.path.append('/u/sbayrak/devel/mapalign/mapalign')
 import embed 
 
-subject = sys.argv[1]
+infile = sys.argv[1]
 # e.g. /ptmp/sbayrak/hcp_prep_out/100307......
+subject = os.path.basename(infile)[0:6]
 
 out_path = '/ptmp/sbayrak/embed_out'
 # set as local output directory (HYDRA)
@@ -29,21 +33,64 @@ def load_matrix(file):
             b[i,:] = np.array(row)
     return b
 
+# load .nii files and calculate correlation matrix (hcp_prep)
+def correlation_matrix(subject):
+    template = 'rfMRI_REST?_??_Atlas_hp2000_clean.dtseries.nii'
+    # template = ('%s/MNINonLinear/Results/rfMRI_REST?_??/rfMRI_REST?_??_Atlas_hp2000_clean.dtseries.nii' % subject)
+    files = [val for val in sorted(glob(os.path.join(subject, template)))]
+    filename = files[:4]
+
+    # read in data and create correlation matrix:
+    # for left hemisphere; 'range(0,nsamples)' for all ??
+    # data_range = range(0,32492) ??
+
+    tmp_t_series = []
+    for x in xrange(0, 4):
+
+        img = nb.load(filename[x])
+        # ntimepoints, nsamples = img.data.shape
+
+        # the following should be a concatenation of all 4 scans from each subject:
+        # brainModels[2] will include both left and right hemispheres
+        # for only left hemisphere: brainModels[1]
+
+        header = img.header.matrix.mims[1].brainModels[2].indexOffset
+        single_t_series = img.data[:, :header].T
+
+        mean_series = single_t_series.mean(axis=0)
+        std_series = single_t_series.std(axis=0)
+
+        tmp_t_series.extend(((single_t_series - mean_series) / std_series).T)
+
+        del img
+        del single_t_series
+
+    # K : matrix of similarities / Kernel matrix / Gram matrix
+    K = (np.corrcoef(np.array(tmp_t_series).T) + 1) / 2.
+    del tmp_t_series
+
+    return K
+
 def save_output(subject, embed_matrix):
-    out_dir = os.path.join(out_path)
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
-    filename = os.path.basename(subject[27:33]) + '_embed_out.csv'
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
+    filename = subject + '_embed_out.csv'
     print filename
-    out_file = os.path.join(out_dir, filename)
+    out_file = os.path.join(out_path, filename)
     # %.e = Floating point exponential format (lowercase)
     np.savetxt(out_file, embed_matrix, fmt='%5.5e', delimiter='\t', newline='\n')
     return out_file
 
-L = load_matrix(subject)
-embedding, result = embed.compute_diffusion_map(L, alpha=0, n_components=20, 
-diffusion_time=0, skip_checks=True, overwrite=True)
+# load correlation matrix from text file
+#L = load_matrix(infile)
 
+# calculate correlation matrices from the subject directory save it
+L = correlation_matrix(infile)
+
+print "correlation matrix:", L.shape
+
+embedding, result = embed.compute_diffusion_map(L, alpha=0, n_components=20,
+    diffusion_time=0, skip_checks=True, overwrite=True)
 save_output(subject, embedding)
 
 print result['lambdas']

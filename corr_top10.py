aa30c9c2c04aeb6126690e50246c47ec60df0d2f
Şeyma Bayrak <sheymaba@gmail.com>
2015-10-15 12:41:15 +0200

    adjustments for memory test on hydra

--- corr_top10.py
+++ corr_top10.py
@@ -9,7 +9,7 @@ import numexpr as ne
 ne.set_num_threads(ne.ncores) # inclusive HyperThreading cores
 import sys
 
-sys.path.append(os.path.expanduser('~/devel/mapalign/mapalign'))
+#sys.path.append(os.path.expanduser('~/devel/mapalign/mapalign'))
 
 import embed
 import load_hcp
@@ -19,13 +19,15 @@ import corr_full
 # here we go ...
 
 # list of all saubjects as numpy array
-#subject_list = np.array(['100307', '912447']) 
-subject_list = np.array(sys.argv)[1:]
+subject_list = np.array(['100307']) #, '912447']) 
+#subject_list = np.array(sys.argv)[1:]
 
-data_path = '/a/documents/connectome/_all'
+#data_path = '/a/documents/connectome/_all'
+data_path = '/ptmp/sbayrak/hcp'
 template = 'MNINonLinear/Results/rfMRI_REST?_??/rfMRI_REST?_??_Atlas_hp2000_clean.dtseries.nii'
 cnt_files = 4
-N_user = None
+N_user = sys.argv[1]
+N_user = int(N_user)
 
 N = len(subject_list)
 
@@ -41,25 +43,25 @@ for i in range(0, N):
     K = corr_faster.corrcoef_upper(K)   
     print "corrcoef data upper triangular shape: ", K.shape
     
-    # get histogram of upper-triangual array
-    dbins = 0.01
-    bins = np.arange(-1, 1+dbins, dbins)
-    x, bins = np.histogram(K, bins)
+    ## get histogram of upper-triangual array
+    #dbins = 0.01
+    #bins = np.arange(-1, 1+dbins, dbins)
+    #x, bins = np.histogram(K, bins)
     
-    # find out threshold value for top 10 percent    
-    ten_percent = 0.10
-    back_sum = 0
+    ## find out threshold value for top 10 percent    
+    #ten_percent = 0.10
+    #back_sum = 0
     
-    for idx in range(x.shape[0]-1, -1, -1):
-        back_sum += x[idx]/float(x.sum())    
-        if back_sum >= ten_percent:
-            thr = bins[idx]
-            print "top-10percent threshold:", thr
-            break
+    #for idx in range(x.shape[0]-1, -1, -1):
+    #    back_sum += x[idx]/float(x.sum())    
+    #    if back_sum >= ten_percent:
+    #        thr = bins[idx]
+    #        print "top-10percent threshold:", thr
+    #        break
 
-    # binarize K via thresholding
-    K[np.where( K >= thr) ] = 1.0    
-    K[np.where( K < thr) ] = 0
+    ## binarize K via thresholding
+    #K[np.where( K >= thr) ] = 1.0    
+    #K[np.where( K < thr) ] = 0
     
     if i == 0:
         SUM = K
@@ -73,25 +75,25 @@ print "loop done"
 # get mean correlation upper triangular
 SUM = ne.evaluate('SUM / N')  
 
-# get full correlation matrix
-N_orig = corr_full.N_original(SUM)
-SUM.resize([N_orig,N_orig])
-corr_full.upper_to_down(SUM)
-print "full-binarized and averaged corrcoef matrix shape: ", SUM.shape 
-
-print "do embed for corr matrix "
-embedding, result = embed.compute_diffusion_map(SUM, alpha=0, n_components=20,
-    diffusion_time=0, skip_checks=True, overwrite=True)
-
-print result['lambdas']
-
-print "embedding done!"    
-        
-# output prefix
-out_prfx="/home/raid/bayrak/tmp/top10_"
-# output precision
-out_prec="%g"
-
-np.savetxt(out_prfx + "embedding.csv", embedding, fmt=out_prec, delimiter='\t', newline='\n')
-np.savetxt(out_prfx + "lambdas.csv", result['lambdas'], fmt=out_prec, delimiter='\t', newline='\n')
-np.savetxt(out_prfx + "vectors.csv", result['vectors'], fmt=out_prec, delimiter='\t', newline='\n')
+## get full correlation matrix
+#N_orig = corr_full.N_original(SUM)
+#SUM.resize([N_orig,N_orig])
+#corr_full.upper_to_down(SUM)
+#print "full-binarized and averaged corrcoef matrix shape: ", SUM.shape 
+#
+#print "do embed for corr matrix "
+#embedding, result = embed.compute_diffusion_map(SUM, alpha=0, n_components=20,
+#    diffusion_time=0, skip_checks=True, overwrite=True)
+#
+#print result['lambdas']
+#
+#print "embedding done!"    
+#        
+## output prefix
+#out_prfx="/home/raid/bayrak/tmp/top10_"
+## output precision
+#out_prec="%g"
+#
+#np.savetxt(out_prfx + "embedding.csv", embedding, fmt=out_prec, delimiter='\t', newline='\n')
+#np.savetxt(out_prfx + "lambdas.csv", result['lambdas'], fmt=out_prec, delimiter='\t', newline='\n')
+#np.savetxt(out_prfx + "vectors.csv", result['vectors'], fmt=out_prec, delimiter='\t', newline='\n')

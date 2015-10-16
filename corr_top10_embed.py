2eb6537a8fcbf50b997223f400fe78334399ae06
Şeyma Bayrak <sheymaba@gmail.com>
2015-10-16 15:18:24 +0200

    embedding on top10 percent corr 1D sums

---
+++ corr_top10_embed.py
@@ -0,0 +1,63 @@
+
+import numpy as np
+import numexpr as ne
+ne.set_num_threads(ne.ncores) # inclusive HyperThreading cores
+
+sys.path.append(os.path.expanduser('~/devel/mapalign/mapalign'))
+sys.path.append(os.path.expanduser('~/devel/hcp_corr'))
+
+import embed
+import load_hcp
+import corr_faster
+import corr_full
+
+
+#A = np.random.randint(5, size=(100, 100))
+
+#A_up = corr_faster.corrcoef_upper(A)
+#corr_faster.write_upper('tmp_A.csv', A_up, fmt="%g")
+
+filenames = ['tmp_A.csv', 'tmp_A.csv',  'tmp_A.csv',  'tmp_A.csv',  'tmp_A.csv',
+        'tmp_A.csv' , 'tmp_A.csv' , 'tmp_A.csv' ]
+
+for i in range(0, len(filenames)):
+    
+    K = np.loadtxt(filenames[i])    
+    
+    if i==0:
+        SUM = K
+    else:
+        SUM = ne.evaluate('SUM + K')
+
+    del K
+    
+print "loading data - loop done"
+   
+# total number of HCP subjects run with corr_top10.py
+N = 476
+# get mean correlation upper triangular
+SUM = ne.evaluate('SUM / N')  
+
+SUM += 1.0
+SUM /= 2.0
+
+# get full correlation matrix
+N_orig = corr_full.N_original(SUM)
+SUM.resize([N_orig,N_orig])
+corr_full.upper_to_down(SUM)
+print "full-binarized and averaged corrcoef matrix shape: ", SUM.shape 
+
+print "do embed for corr matrix "
+
+embedding, result = embed.compute_diffusion_map(SUM, alpha=0, n_components=20,
+    diffusion_time=0, skip_checks=True, overwrite=True)
+
+print result['lambdas']
+
+print "embedding done!"    
+ 
+out_prfx = "tmp_"
+out_prec = "%g"       
+np.savetxt(out_prfx + "embedding.csv", embedding, fmt=out_prec, delimiter='\t', newline='\n')
+np.savetxt(out_prfx + "lambdas.csv", result['lambdas'], fmt=out_prec, delimiter='\t', newline='\n')
+np.savetxt(out_prfx + "vectors.csv", result['vectors'], fmt=out_prec, delimiter='\t', newline='\n')

be1b382f0118275133fe621f7d353548ca920f40
Ruediger Meier <ruediger.meier@ga-group.nl>
2015-10-26 14:24:17 +0100

    corr_top10_embed: minor cleanup style and debug messages

--- corr_top10_embed.py
+++ corr_top10_embed.py
@@ -26,18 +26,19 @@ args = parser.parse_args()
 filenames = np.array(args.sum_file)
 
 for i in range(0, len(filenames)):
-    print "loop", i    
+    print "do loop %d/%d, %s" % (i+1, len(filenames), filenames[i])
     K = hcp_util.load_vector(filenames[i])
-    
+
     if i==0:
+        print "loaded matrix shape:", K.shape
         SUM = K
     else:
         SUM = ne.evaluate('SUM + K')
 
     del K
-    
+
 print "loading data - loop done"
-   
+
 # total number of HCP subjects run with corr_top10.py
 N = args.cntsubjects
 # get mean correlation upper triangular
@@ -48,9 +49,7 @@ N_orig = hcp_util.N_original(SUM)
 SUM.resize([N_orig,N_orig])
 hcp_util.upper_to_down(SUM)
 
-#print "full-binarized and averaged corrcoef matrix shape: ", SUM.shape 
-
-print "top left quadrant (binarized, averaged, corrcoef mtx) shape: ", SUM.shape 
+print "averaged corrcoef matrix shape: ", SUM.shape
 
 print "do embed for corr matrix "
 
@@ -58,11 +57,10 @@ embedding, result = embed.compute_diffusion_map(SUM, alpha=0, n_components=20,
     diffusion_time=0, skip_checks=True, overwrite=True)
 
 print result['lambdas']
+print "embedding done!"
 
-print "embedding done!"    
- 
 out_prfx = args.outprfx
-out_prec = "%g"       
+out_prec = "%g"
 np.savetxt(out_prfx + "embedding.csv", embedding, fmt=out_prec, delimiter='\t', newline='\n')
 np.savetxt(out_prfx + "lambdas.csv", result['lambdas'], fmt=out_prec, delimiter='\t', newline='\n')
 np.savetxt(out_prfx + "vectors.csv", result['vectors'], fmt=out_prec, delimiter='\t', newline='\n')

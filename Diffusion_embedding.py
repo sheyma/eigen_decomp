334b9ddc3729dbf0a798a34fe28561c3df51ed58
Şeyma Bayrak <sheymaba@gmail.com>
2015-10-29 17:07:06 +0100

    save Daniel's embedding result

--- Diffusion_embedding.py
+++ Diffusion_embedding.py
@@ -3,18 +3,25 @@ import sys, numpy as np, h5py
 import os
 sys.path.append("utils_py/mapalign")
 sys.path.append(os.path.expanduser('~/devel/mapalign'))
-from mapalign import embed, dist, align
+import embed
 
-A = h5py.File('matrix_hcp_cortex_lh.mat','r') 
+A = h5py.File('/ptmp/sbayrak/test_daniel/matrix_hcp_cortex_lh.mat','r') 
 L = A.get('matrix') 
 L = np.array(L)
-cortex = A.get('cortex') 
-cortex = np.array(cortex)
+#cortex = A.get('cortex') 
+#cortex = np.array(cortex)
 
 keep = list(np.where(sum(L) != 0))
 
 embedding, result = embed.compute_diffusion_map(L[keep].T[keep].T, alpha=0.5, n_components=5, diffusion_time=0,
                           skip_checks=False, overwrite=False)
 
-from scipy.io import savemat
-savemat('embed_hcp_cortex.lh.mat',{'embedding': embedding, 'result': result, 'keep': keep})
+print result['lambdas']
+print "embedding done!"
+
+
+out_pfrx = "/ptmp/sbayrak/test_daniel_"
+out_prec = "%g"
+np.savetxt(out_pfrx + "embedding_01.csv", embedding, fmt=out_prec, delimiter='\t', newline='\n')
+np.savetxt(out_pfrx + "lambdas_01.csv", result['lambdas'], fmt=out_prec, delimiter='\t', newline='\n')
+np.savetxt(out_pfrx + "vectors_01.csv", result['vectors'], fmt=out_prec, delimiter='\t', newline='\n')

103d38e56a5244c2e3d7d3785591d0bf7d9ded49
Şeyma Bayrak <sheymaba@gmail.com>
2015-10-29 10:57:47 +0100

    remove embedding related lines, remove 'SUM.csv' part from outprf

--- corr_top10.py
+++ corr_top10.py
@@ -10,10 +10,8 @@ ne.set_num_threads(ne.ncores) # inclusive HyperThreading cores
 import sys
 import argparse
 
-sys.path.append(os.path.expanduser('~/devel/mapalign/mapalign'))
 sys.path.append(os.path.expanduser('~/devel/hcp_corr'))
 
-import embed
 import hcp_util
 
 # here we go ...
@@ -129,26 +127,5 @@ out_prfx=args.outprfx
 out_prec="%g"
 
 # write out averaged upper triangular
-hcp_util.write_upper(out_prfx + "SUM.csv", SUM, fmt=out_prec)
+hcp_util.write_upper(out_prfx, SUM, fmt=out_prec)
 
-# get mean correlation upper triangular
-#SUM = ne.evaluate('SUM / N')  
-
-## get full correlation matrix
-#N_orig = hcp_util.N_original(SUM)
-#SUM.resize([N_orig,N_orig])
-#hcp_util.upper_to_down(SUM)
-#print "full-binarized and averaged corrcoef matrix shape: ", SUM.shape 
-
-#print "do embed for corr matrix "
-
-#embedding, result = embed.compute_diffusion_map(SUM, alpha=0, n_components=20,
-#    diffusion_time=0, skip_checks=True, overwrite=True)
-
-#print result['lambdas']
-
-#print "embedding done!"    
-        
-#np.savetxt(out_prfx + "embedding.csv", embedding, fmt=out_prec, delimiter='\t', newline='\n')
-#np.savetxt(out_prfx + "lambdas.csv", result['lambdas'], fmt=out_prec, delimiter='\t', newline='\n')
-#np.savetxt(out_prfx + "vectors.csv", result['vectors'], fmt=out_prec, delimiter='\t', newline='\n')

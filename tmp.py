2ae80204d15cf6bf6a3803b92c85bc60c226f33c
Şeyma Bayrak <sheymaba@gmail.com>
2015-10-30 23:27:57 +0100

    numpy's percentile method..

--- tmp.py
+++ tmp.py
@@ -1,17 +1,20 @@
 import sys
 import os
 import matplotlib.pylab as pl
+import numpy as np
 
 sys.path.append(os.path.expanduser('~/devel/hcp_corr'))
 
 import hcp_util
 
 cnt_files = 4
-subject = '/tmp/sbayrak/hcp/907656'
-N_first = 0
-N_cnt = 100
+subject = '/var/tmp/hcp/907656'
 
-K_norm = hcp_util.t_series(subject, cnt_files=cnt_files, N_first=N_first, N_cnt=N_cnt)
+
+N_first = 0
+N_cnt = None
+
+#K_norm = hcp_util.t_series(subject, cnt_files=cnt_files, N_first=N_first, N_cnt=N_cnt)
 
 K_none = hcp_util.t_series(subject,
              template = None,
@@ -20,11 +23,40 @@ K_none = hcp_util.t_series(subject,
              N_first=N_first,
              subject_path=None,
              dtype=None,
-             normalize=False)
-pl.figure(1); 
-pl.plot(K_norm)
+             normalize=True)
+print K_none.shape
 
-pl.figure(2);
-pl.plot(K_none)
+n = K_none.shape[0]
+m = K_none.shape[1]
 
+pl.figure(2); 
+for i in range(500, 550 ):
+    pl.plot(K_none[i,:])
+pl.title('907656')
+pl.xlabel('t')
+pl.ylabel('node (first 50)')
+#pl.figure(2);
+#pl.plot(K_none)
 
+A = np.random.randn(60, 60)
+
+A = np.corrcoef(A)
+
+A = A.flatten()
+
+ten_percent = 0.1
+dbins = 0.01
+bins = np.arange(-1, 1+dbins, dbins)
+x, bins = np.histogram(A, bins)
+# find out threshold value for top 10 percent
+back_sum = 0
+for idx in range(x.shape[0]-1, -1, -1):
+    back_sum += x[idx]/float(x.sum())
+    if back_sum >= ten_percent:
+        thr = bins[idx]
+        print "top-10percent threshold:", thr
+        break
+ 
+np.percentile(A, 90)
+
+ 

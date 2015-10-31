af74b053937aa6b05958ae192479bdc58f9effa7
Şeyma Bayrak <sheymaba@gmail.com>
2015-10-31 02:08:43 +0100

    I/O with HDF5 format

--- corr_top10_embed.py
+++ corr_top10_embed.py
@@ -9,7 +9,7 @@ sys.path.append(os.path.expanduser('~/devel/mapalign/mapalign'))
 sys.path.append(os.path.expanduser('~/devel/hcp_corr'))
 
 import embed
-import hcp_util
+import h5py
 
 # here we go ...
 
@@ -27,40 +27,43 @@ filenames = np.array(args.sum_file)
 
 for i in range(0, len(filenames)):
     print "do loop %d/%d, %s" % (i+1, len(filenames), filenames[i])
-    K = hcp_util.load_vector(filenames[i])
-
+    #K = hcp_util.load_vector(filenames[i])
+    
+    K_init = h5py.File(filenames[i], 'r')
+    K = K_init.get('sum')
+    np.array(K)
+   
     if i==0:
         print "loaded matrix shape:", K.shape
         SUM = K
     else:
         SUM = ne.evaluate('SUM + K')
-
-    del K
+    
+    #del A
 
 print "loading data - loop done"
 
 # total number of HCP subjects run with corr_top10.py
 N = args.cntsubjects
-# get mean correlation upper triangular
+# get mean correlation 
 SUM = ne.evaluate('SUM / N')  
 
-# get full correlation matrix
-N_orig = hcp_util.N_original(SUM)
-SUM.resize([N_orig,N_orig])
-hcp_util.upper_to_down(SUM)
+print "writing-out GROUP-LEVEL data in HDF5 format"
+h = h5py.File(args.outprfx, 'w')
+h.create_dataset('sum', data=SUM)
 
-print "averaged corrcoef matrix shape: ", SUM.shape
-
-print "do embed for corr matrix "
-
-embedding, result = embed.compute_diffusion_map(SUM, alpha=0, n_components=20,
-    diffusion_time=0, skip_checks=True, overwrite=True)
-
-print result['lambdas']
-print "embedding done!"
-
-out_prfx = args.outprfx
-out_prec = "%g"
-np.savetxt(out_prfx + "embedding.csv", embedding, fmt=out_prec, delimiter='\t', newline='\n')
-np.savetxt(out_prfx + "lambdas.csv", result['lambdas'], fmt=out_prec, delimiter='\t', newline='\n')
-np.savetxt(out_prfx + "vectors.csv", result['vectors'], fmt=out_prec, delimiter='\t', newline='\n')
+print "group-level matrix shape: ", SUM.shape
+#
+#print "do embed for corr matrix "
+#
+#embedding, result = embed.compute_diffusion_map(SUM, alpha=0, n_components=20,
+#    diffusion_time=0, skip_checks=True, overwrite=True)
+#
+#print result['lambdas']
+#print "embedding done!"
+#
+#out_prfx = args.outprfx
+#out_prec = "%g"
+#np.savetxt(out_prfx + "embedding.csv", embedding, fmt=out_prec, delimiter='\t', newline='\n')
+#np.savetxt(out_prfx + "lambdas.csv", result['lambdas'], fmt=out_prec, delimiter='\t', newline='\n')
+#np.savetxt(out_prfx + "vectors.csv", result['vectors'], fmt=out_prec, delimiter='\t', newline='\n')

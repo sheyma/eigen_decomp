5ac21e645585bef2c79c30a10c635621994e8c69
Şeyma Bayrak <sheymaba@gmail.com>
2015-11-14 11:15:18 +0100

    load t_series, get corr-mtx and simi-mtx, do embedding

--- hcp_prep.py
+++ hcp_prep.py
@@ -1,83 +1,57 @@
-# read-in cifti (nifti2) format via Satra's nibabel repository:
-# $git clone --branch enh/cifti2 https://github.com/satra/nibabel.git
-
-from glob import glob
-import os
 import numpy as np
-import nibabel as nb
+import hcp_corr
 import sys
+import os
+import h5py
+sys.path.append(os.path.expanduser('~/devel/mapalign/mapalign'))
+import embed
+import argparse
 
-print "python version: ", sys.version[0:5]
-# (HYDRA) 2.7.9
-print "numpy version: ", np.__version__
-# (HYDRA) 1.9.1
+## begin parse command line arguments
+parser = argparse.ArgumentParser()
 
-# # set as a local input directory (MPI)
-# data_path = '/a/documents/connectome/_all'
-# # set a list for the subject ID's
-# subject_list = ['100307', '100408', '101006', '101107', '101309']
-# # set as local output directory (MPI)
-# out_path = '/home/raid/bayrak/devel/eigen_decomp/hcp_prep_out'
+parser.add_argument('--N_first', default=None, type=int)
+parser.add_argument('--N_cnt', default=None, type=int)
+# output prefix, e.g. /ptmp/sbayrak/corr_top10_out/top10_
+parser.add_argument('-o', '--outprfx', required=True)
+# the rest args are the subject path(s), e.g. /ptmp/sbayrak/hcp/*
+parser.add_argument("subject",nargs="+")
 
-subject = sys.argv[1] # e.g. /ptmp/mdani/hcp/100307
-# set as local output directory (HYDRA)
-out_path = '/ptmp/sbayrak/hcp_prep_out'
+args = parser.parse_args()
+## end parse command line arguments
 
-def correlation_matrix(subject):
-    template = 'rfMRI_REST?_??_Atlas_hp2000_clean.dtseries.nii'
-    # template = ('%s/MNINonLinear/Results/rfMRI_REST?_??/rfMRI_REST?_??_Atlas_hp2000_clean.dtseries.nii' % subject)
-    files = [val for val in sorted(glob(os.path.join(subject, template)))]
-    filename = files[:4]
+subject_list = np.array(args.subject)
 
-    # read in data and create correlation matrix:
-    # for left hemisphere; 'range(0,nsamples)' for all ??
-    # data_range = range(0,32492) ??
+N = len(subject_list)
 
-    tmp_t_series = []
-    for x in xrange(0, 4):
+for i in range(0, N):
+    subject = subject_list[i]
+    print "do loop %d/%d, %s" % (i+1, N, subject)
+    # load time-series matrix
+    K = hcp_corr.t_series(subject, N_first=args.N_first, N_cnt=args.N_cnt)
+    # get upper triangular of corr matrix
+    K = hcp_corr.corrcoef_upper(K)
+    # convert upper triangulat into full corr matrix
+    N_orig = hcp_corr.N_original(K)
+    K.resize([N_orig, N_orig])
+    K = hcp_corr.upper_to_down(K)
+    print "corr matrix shape ", K.shape    
+    # get similarity matrix (Kernel matrix / Gram matrix)
+    K +=1.0
+    K /=2.0
+    # do embedding on similarity matrix
+    print "do embedding..."
+    embedding, result = embed.compute_diffusion_map(K, alpha=0.5, 
+                                                    n_components=10, skip_checks=True)
 
-        img = nb.load(filename[x])
-        # ntimepoints, nsamples = img.data.shape
+    out_prf = args.outprfx
+    out_sbj = os.path.basename(subject)
+    # write out embedding components, eigenvalues and eigenvectors
+    print "writing out embedding results..."
+    h = h5py.File(out_prf + out_sbj + '_embedding.h5', 'w')
+    h.create_dataset('embedding', data=embedding)
+    h.create_dataset('lambdas', data=result['lambdas'])
+    h.create_dataset('vectors', data=result['vectors'])
+    h.close()
 
-        # the following should be a concatenation of all 4 scans from each subject:
-        # brainModels[2] will include both left and right hemispheres
-        # for only left hemisphere: brainModels[1]
-
-        header = img.header.matrix.mims[1].brainModels[2].indexOffset
-        single_t_series = img.data[:, :header].T
-
-        mean_series = single_t_series.mean(axis=0)
-        std_series = single_t_series.std(axis=0)
-
-        tmp_t_series.extend(((single_t_series - mean_series) / std_series).T)
-
-        del img
-        del single_t_series
-
-    # K : matrix of similarities / Kernel matrix / Gram matrix
-    K = (np.corrcoef(np.array(tmp_t_series).T) + 1) / 2.
-    del tmp_t_series
-
-    return K
-
-def save_output(subject, matrix):
-    out_dir = os.path.join(out_path)
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
-    filename = os.path.basename(subject) + '_hcp_prep_out_COMP.csv'
-    print filename
-    out_file = os.path.join(out_dir, filename)
-    # %.e = Floating point exponential format (lowercase)
-    np.savetxt(out_file, matrix, fmt='%5.5e', delimiter='\t', newline='\n')
-    return out_file
-
-## calculate correlation matrices for the subject directory save it
-#K = correlation_matrix(subject)
-#save_output(subject, K)
-
-import load_nifti
-template = 'rfMRI_REST?_??_Atlas_hp2000_clean.dtseries.nii'
-K = load_nifti.load_nii(subject, template, 4)
-L = np.corrcoef(K.T)
-del K
-save_output(subject, L)
+print "loop done!"

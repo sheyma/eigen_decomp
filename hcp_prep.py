fce28adf62d11998dd14a784d1533148fb48a89a
Şeyma Bayrak <sheymaba@gmail.com>
2015-11-17 03:20:31 +0100

    add right hemisphere option, ignore NaN entries in individuals

--- hcp_prep.py
+++ hcp_prep.py
@@ -1,3 +1,12 @@
+"""
+load t-series
+get correlation matrix
+get similarity matrix
+do embedding
+
+"""
+
+
 import numpy as np
 import hcp_corr
 import sys
@@ -10,6 +19,8 @@ import argparse
 ## begin parse command line arguments
 parser = argparse.ArgumentParser()
 
+# left right or both hemispheres ...
+parser.add_argument('--hem', default='LH', choices=['full','LH','RH'])
 parser.add_argument('--N_first', default=None, type=int)
 parser.add_argument('--N_cnt', default=None, type=int)
 # output prefix, e.g. /ptmp/sbayrak/corr_top10_out/top10_
@@ -29,7 +40,8 @@ for i in range(0, N):
     subject_basename = os.path.basename(subject)
     print "do loop %d/%d, %s" % (i+1, N, subject)
     # load time-series matrix
-    K = hcp_corr.t_series(subject, N_first=args.N_first, N_cnt=args.N_cnt)
+    K = hcp_corr.t_series(subject,  hemisphere=args.hem,
+                          N_first=args.N_first, N_cnt=args.N_cnt)
     # get upper triangular of corr matrix
     K = hcp_corr.corrcoef_upper(K)
     # convert upper triangulat into full corr matrix
@@ -40,9 +52,15 @@ for i in range(0, N):
     # get similarity matrix (Kernel matrix / Gram matrix)
     K +=1.0
     K /=2.0
+    
+    # set NaN entries to 0
+    K[np.where(np.isnan(K) == True)] = 0
+    # ignore zero entries?
+    ind = np.where(np.sum(K,axis=1) != 1)     
+    
     # do embedding on similarity matrix
     print "do embedding..."
-    embedding, result = embed.compute_diffusion_map(K, 
+    embedding, result = embed.compute_diffusion_map(K[ind].T[ind].T, 
                                                     n_components=10)
 
     # write out embedding components, eigenvalues and eigenvectors

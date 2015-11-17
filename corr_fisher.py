a76e5af80dfb4235c231679e821d51136eef27ac
Şeyma Bayrak <sheymaba@gmail.com>
2015-11-17 06:23:06 +0100

    fisher step 1: summing only Fisherr2z results

--- corr_fisher.py
+++ corr_fisher.py
@@ -3,11 +3,26 @@ import numpy as np
 import numexpr as ne
 ne.set_num_threads(ne.ncores) # inclusive HyperThreading cores
 import sys
+import argparse
 
 sys.path.append(os.path.expanduser('~/devel/mapalign/mapalign'))
 
-import embed
+import h5py
 import hcp_corr
+
+## parse command line arguments
+parser = argparse.ArgumentParser()
+# left right or both hemispheres ...
+parser.add_argument('--hem', default='LH', choices=['full','LH','RH'])
+# output prefix, e.g. /ptmp/sbayrak/corr_top10_out/top10_
+parser.add_argument('--N_first', default=None, type=int)
+parser.add_argument('--N_cnt', default=None, type=int)
+# output prefix, e.g. /ptmp/sbayrak/corr_top10_out/top10_
+parser.add_argument('-o', '--outprfx', required=True)
+# the rest args are the subject path(s), e.g. /ptmp/sbayrak/hcp/*
+parser.add_argument("subject",nargs="+")
+args = parser.parse_args()
+
 
 def fisher_r2z(R):
     return ne.evaluate('arctanh(R)')
@@ -16,16 +31,11 @@ def fisher_z2r(Z):
     X = ne.evaluate('exp(2*Z)')
     return ne.evaluate('(X - 1) / (X + 1)')
 
-# here we go ...
-
-## parse command line arguments
 # first arg is output prefix, e.g. /ptmp/sbayrak/fisher/fisher_
-cliarg_out_prfx = sys.argv[1]
-# the rest args are the subject path(s), e.g. /ptmp/sbayrak/hcp/*
-cliarg_rest = sys.argv[2:]
+cliarg_out_prfx = args.outprfx
 
 # list of all subjects as numpy array
-subject_list = np.array(cliarg_rest) # e.g. /ptmp/sbayrak/hcp/*
+subject_list = np.array(args.subject) # e.g. /ptmp/sbayrak/hcp/*
 
 cnt_files = 4
 N_user = None
@@ -37,13 +47,15 @@ for i in range(0, N):
     print "do loop %d/%d, %s" % (i+1, N, subject)
 
     # load time-series matrix of the subject    
-    K = hcp_corr.t_series(subject, cnt_files=cnt_files, N_cnt=N_user)
-
+    K = hcp_corr.t_series(subject, hemisphere=args.hem, N_first=args.N_first,
+                          N_cnt=args.N_cnt)
+    
     # get upper-triangular of correlation matrix of time-series as 1D array
     K = hcp_corr.corrcoef_upper(K)
     print "corrcoef data shape: ", K.shape
 
     # Fisher r to z transform on the correlation upper triangular
+    print "do Fisher r2z..."    
     K = fisher_r2z(K)
         
     # sum all Fisher transformed 1D arrays
@@ -54,38 +66,14 @@ for i in range(0, N):
 
     del K
 
+print "SUM shape: ", SUM.shape
 print "loop done"
 
-# get average 
-SUM = ne.evaluate('SUM / N')
-
-# Fisher z to r transform on average , now this is back to correlation array
-SUM = fisher_z2r(SUM)
-
-# transform correlation array into similarity array
-SUM += 1.0
-SUM /= 2.0
-
-# get full similartiy matrix of correlations
-N = hcp_corr.N_original(SUM)
-SUM.resize([N,N])
-hcp_corr.upper_to_down(SUM)
-
-print "SUM.shape", SUM.shape
-
-print "do embed for correlation matrix:", SUM.shape
-
-# Satra's embedding algorithm 
-embedding, result = embed.compute_diffusion_map(SUM, alpha=0, n_components=20,
-    diffusion_time=0, skip_checks=True, overwrite=True)
-
 # output prefix
-out_prfx=cliarg_out_prfx
-# output precision
-out_prec="%g"
+out_prfx=args.outprfx
 
-np.savetxt(out_prfx + "embedding2.csv", embedding, fmt=out_prec, delimiter='\t', newline='\n')
-np.savetxt(out_prfx + "lambdas2.csv", result['lambdas'], fmt=out_prec, delimiter='\t', newline='\n')
-np.savetxt(out_prfx + "vectors2.csv", result['vectors'], fmt=out_prec, delimiter='\t', newline='\n')
-
-print result['lambdas']
+# write-out full matrix in HDF5 format
+print "writing-out data in HDF5 format"
+h = h5py.File(out_prfx, 'w')
+h.create_dataset('sum', data=SUM)
+h.close()

84f8be7ddb75191cc646b751f53ba6fa5210f470
Şeyma Bayrak <sheymaba@gmail.com>
2015-12-11 15:57:45 +0100

    write out realigned with subject id's

--- embed_align.py
+++ embed_align.py
@@ -13,16 +13,21 @@ import hcp_corr
 # Load all embedding variables across subjects as arrays:
 embeddings = [] 
 filelist = []
+i = 1
 for f in glob.glob('/nobackup/kocher1/bayrak/hcp_embed/embedding_*.h5'):
     d = np.array(h5py.File(f, 'r').get('embedding'))
-    if 29696 == d.shape[0]:
+    if (29696 == d.shape[0] and i < 11):
         embeddings.append(d)
         filelist.append(f)
-    else: 
-        print f
-        
+        print os.path.basename(f)        
+        #print d.shape[0]        
+        i += 1
+    #else: 
+        #print "bad subject: ", os.path.basename(f)
+        #os.path.basename(f)
+        #print f
 # Realign embeddings across subjects
-realigned, xfms = align.iterative_alignment_with_coords(embeddings10, 
+realigned, xfms = align.iterative_alignment_with_coords(embeddings, 
                                                         coords=None, 
                                                         n_iters=1, 
                                                         n_samples=0.1, 

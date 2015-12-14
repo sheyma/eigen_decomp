a7599f22e0e20eca454ea8aff8b9d5eb1eeebbbe
Şeyma Bayrak <sheymaba@gmail.com>
2015-12-14 11:52:17 +0100

    parse the subject list, remove saving out part

--- embed_align.py
+++ embed_align.py
@@ -8,24 +8,29 @@ Created on Thu Dec 10 14:03:07 2015
 import os, argparse, sys, h5py, numpy as np, glob
 sys.path.append(os.path.expanduser('~/devel/mapalign/mapalign'))
 import embed, align
-import hcp_corr
 
 # Load all embedding variables across subjects as arrays:
 embeddings = [] 
 filelist = []
-i = 1
-for f in glob.glob('/nobackup/kocher1/bayrak/hcp_embed/embedding_*.h5'):
-    d = np.array(h5py.File(f, 'r').get('embedding'))
-    if (29696 == d.shape[0] and i < 11):
+
+# parse command line arguments for embedding components
+parser = argparse.ArgumentParser()
+parser.add_argument("subject",nargs="+")
+args = parser.parse_args()
+
+subject_list = args.subject
+
+# get a list of embeddings for subjects
+for f in subject_list:
+    d =  np.array(h5py.File(f, 'r').get('embedding'))   
+    if (29696 == d.shape[0]):
         embeddings.append(d)
         filelist.append(f)
-        print os.path.basename(f)        
-        #print d.shape[0]        
-        i += 1
-    #else: 
-        #print "bad subject: ", os.path.basename(f)
-        #os.path.basename(f)
-        #print f
+    else:
+        print "bad subject", f
+
+print "listed embedding input shape: ", np.shape(embeddings)
+
 # Realign embeddings across subjects
 realigned, xfms = align.iterative_alignment_with_coords(embeddings, 
                                                         coords=None, 

49a3738c5b993070226e7446f263bb701feef58a
Şeyma Bayrak <sheymaba@gmail.com>
2016-01-07 15:42:33 +0100

    read in subjects from *csv list

--- embed_align.py
+++ embed_align.py
@@ -8,6 +8,8 @@ Created on Thu Dec 10 14:03:07 2015
 import os, argparse, sys, h5py, numpy as np, glob
 sys.path.append(os.path.expanduser('~/devel/mapalign/mapalign'))
 import embed, align
+import pandas as pd
+
 
 # Load all embedding variables across subjects as arrays:
 embeddings = [] 
@@ -15,11 +17,11 @@ filelist = []
 
 # parse command line arguments for embedding components
 parser = argparse.ArgumentParser()
-parser.add_argument("subject",nargs="+")
+#parser.add_argument("subject",nargs="+")
 parser.add_argument('-o', '--outprfx', required=True)
 args = parser.parse_args()
 
-subject_list = args.subject
+#subject_list = args.subject
 
 #subject_list = ['/nobackup/kocher1/bayrak/test/100307_test.h5',
 #                '/nobackup/kocher1/bayrak/test/100408_test.h5',
@@ -35,24 +37,38 @@ subject_list = args.subject
 #    else:
 #        print "bad subject", f
 
-for f in subject_list:
-    d =  np.array(h5py.File(f, 'r').get('embedding'))   
-    if (29696 == d.shape[0]):
-        embeddings.append(d)
-        filelist.append(f)
-    else:
-        print "bad subject", f
+#for f in subject_list:
+#    d =  np.array(h5py.File(f, 'r').get('embedding'))   
+#    if (29696 == d.shape[0]):
+#        embeddings.append(d)
+#        filelist.append(f)
+#    else:
+#        print "bad subject", f
+
+subfile = pd.read_csv('/nobackup/kocher1/bayrak/subject_list.csv', header=None)
+embeddings = []
+for index, row in subfile.iterrows():
+	S = h5py.File('/nobackup/kocher1/bayrak/hcp_embed/embedding_%s.h5' % str(row[0]) , 'r').get('embedding')
+	embeddings.append(np.array(S))
+	print row[0]
+
 
 
 print "listed embedding input shape: ", np.shape(embeddings)
 
-# Realign embeddings across subjects
-realigned, xfms = align.iterative_alignment_with_coords(embeddings, 
-                                                        coords=None, 
-                                                        n_iters=1, 
-                                                        n_samples=0.1, 
-                                                        use_mean=False)
-                                                        
+print "iterative alignment starts..."
+
+realigned, xfms = align.iterative_alignment(embeddings, n_iters=10)
+
+print "iterative alignment finishes..."
+print "realigned shape: ", np.shape(realigned)
+## Realign embeddings across subjects
+#realigned, xfms = align.iterative_alignment_with_coords(embeddings, 
+#                                                        coords=None, 
+#                                                        n_iters=1, 
+#                                                        n_samples=0.1, 
+#                                                        use_mean=False)
+#                                                        
 # output prefix
 out_prfx=args.outprfx
 

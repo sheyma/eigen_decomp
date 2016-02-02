ae7ef836d1e8c9bc93bb71f757b283b1d06d674d
Şeyma Bayrak <sheymaba@gmail.com>
2016-02-02 14:37:42 +0100

    smoothing numpy arrays

---
+++ smoothing.py
@@ -0,0 +1,41 @@
+import h5py
+import numpy as np
+import pandas as pd
+import matplotlib.pylab as plt
+
+tmp = np.array(h5py.File('/home/sheyma/tmp/embedding_672756.h5', 'r').get('embedding'))
+
+tmp = tmp[0:278,0]
+
+plt.figure(1); plt.plot(tmp)
+
+
+def smooth(x,window_len=11,window='hanning'):
+    """smooth the data using a window with requested size.
+
+
+    TODO: the window parameter could be the window itself if an array instead of a string
+    NOTE: length(output) != length(input), to correct this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
+    """
+
+    if x.ndim != 1:
+        raise ValueError, "smooth only accepts 1 dimension arrays."
+
+    if x.size < window_len:
+        raise ValueError, "Input vector needs to be bigger than window size."
+
+    if window_len<3:
+        return x
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+
+    s=np.r_[x[window_len-1:0:-1],x,x[-1:-window_len:-1]]
+    #print(len(s))
+    if window == 'flat': #moving average
+        w=numpy.ones(window_len,'d')
+    else:
+        w=eval('numpy.'+window+'(window_len)')
+
+    y=numpy.convolve(w/w.sum(),s,mode='valid')
+    return y

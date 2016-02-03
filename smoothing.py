c628d45b1c964ddd77e6c5a7f9461760b482eeda
Şeyma Bayrak <sheymaba@gmail.com>
2016-02-03 16:40:22 +0100

    add path varible, read-in subject_list,csv, read-in aligned components

--- smoothing.py
+++ smoothing.py
@@ -1,21 +1,31 @@
 import h5py
 import numpy as np
-import pandas as pd
+import csv
 import matplotlib.pylab as plt
 from scipy.ndimage.filters import gaussian_filter1d
 
+path = '/ptmp/sbayrak/tmp/'
+subject_list = []
+with open(path + 'subject_list.csv', 'rb') as f:
+    reader = csv.reader(f);
+    subject_list = list(reader);
 
-#tmp = np.array(h5py.File('/home/sheyma/tmp/embedding_672756.h5', 'r').get('embedding'))
-tmp = np.array(h5py.File('/home/raid/bayrak/tmp/embeddings_full_901038.h5', 'r').get('embedding'))
+random_int = np.random.permutation(len(subject_list))[0]
+subject_id = subject_list[random_int] 
+subject_id = ''.join(subject_id)
+print "chosen HCP subject : ", subject_id
 
-tmp = tmp[0:278,0]
-
+# take the first (aligned) component of a random subject
+aligned_all = h5py.File(path + '468_alignments.h5', 'r')
+tmp = aligned_all[subject_id]['aligned']
+tmp = np.array(tmp)
+tmp = tmp[:,0]
 
 
 def smooth(x, window_len=11, window='hanning'):
     """smooth 1D data using a window with requested size.
 
-    window_len : int, odd number, choose as 20% of the sample size
+    window_len : int, odd number, choose as [1, 4]% of the sample size
     
     Reference :  http://scipy-cookbook.readthedocs.org/items/SignalSmooth.html
     http://stackoverflow.com/questions/25216382/gaussian-filter-in-scipy
@@ -56,10 +66,18 @@ def smooth(x, window_len=11, window='hanning'):
 
     return y
 
-     
-   
-TMP = smooth(tmp, window_len = 11, window='gaussian')
-plt.figure(2); plt.plot(tmp, 'b'); 
-plt.plot(TMP, 'r', label='window_len=5')
-plt.title('blackman window - 50 data points')
+window = 'bartlett'
+plt.figure(1); plt.plot(tmp, 'b');
+plt.title(window)
+window_lengths = [0.005, 0.01, 0.02, 0.03, 0.04]     
+colors = 'rgcyk'
+j = 0;
+for i in window_lengths:
+    window_len = i * len(tmp)
+    TMP = smooth(tmp, window_len, window)
+    plt.plot(TMP, colors[j], label=str(i))
+    j += 1    
+    print window_len    
+    
 plt.legend()
+plt.show()

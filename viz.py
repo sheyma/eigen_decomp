64b8d5e82fbda4686ddb90dce9e6d8f24b3acf5c
Şeyma Bayrak <sheymaba@gmail.com>
2016-02-04 03:58:29 +0100

    plot mean of modes over subjects

--- viz.py
+++ viz.py
@@ -32,10 +32,21 @@ def choose_random_subject(subject_list):
     return subject_id
 
 def choose_component(DATA, subject_id, component = 0, mode = 'aligned'):
+    # choose a component of given subject    
     A = DATA[subject_id][mode]
     A = np.array(A)
     A = A[:, component]
     return A
+    
+def get_mean(DATA, subject_list, component = 0, mode = 'aligned'):
+    # get mean of a component over all subjects    
+    DATA_all = []    
+    for subject_id in subject_list:
+        subject_id = ''.join(subject_id)
+        tmp = np.array(DATA[subject_id]['aligned'])[:,component]
+        DATA_all.append(tmp)
+    DATA_mean = np.mean(DATA_all, axis=0)
+    return DATA_mean
     
 def get_surface(surface_data, hemisphere, surface_type):
     """
@@ -67,52 +78,38 @@ n, vertices, triangles = get_surface(surface_data, hemisphere, surface_type)
 
 DATA = h5py.File(path + '468_alignments.h5', 'r')
 
-# plot several subjects
-for subject_id in subject_list[0:100]:
-    ## chose subject_id randomly     
-    #subject_id = choose_random_subject(subject_list)
-    #subject_id = '100307'
-    subject_id = ''.join(subject_id)
+# plot subjects individually
+#for subject_id in subject_list:
+#    ## chose subject_id randomly     
+#    #subject_id = choose_random_subject(subject_list)
+#    #subject_id = '100307'
+#    subject_id = ''.join(subject_id)
+#    component = 0
+#    mode = 'aligned'
+#    subject_component = choose_component(DATA, subject_id, component, mode)
+#    data = np.zeros(len(vertices))
+#    data[n] = subject_component
+#    plt = plotting.plot_surf_stat_map(vertices, triangles, stat_map=data, cmap='jet', azim=0)
+#    import matplotlib.pyplot as plt
+#    plt.title(subject_id + ' , component ' + str(component+1))
+#    plt.savefig(path_out + subject_id + '_comp_' + str(component+1)+ '.png')
+
+
+# plot a mean component over all subjects
+components = np.arange(0, 10, 1)
+for component in components:
     component = 0
     mode = 'aligned'
-    subject_component = choose_component(DATA, subject_id, component, mode)
+    DATA_mean = get_mean(DATA, subject_list, component=component, mode=mode)
     data = np.zeros(len(vertices))
-    data[n] = subject_component
-    plt = plotting.plot_surf_stat_map(vertices, triangles, stat_map=data, cmap='jet', azim=0)
+    data[n] = DATA_mean
+    plotting.plot_surf_stat_map(vertices, triangles, stat_map=data, cmap='jet', azim=0)
     import matplotlib.pyplot as plt
-    plt.title(subject_id + ' , component ' + str(component+1))
-    plt.savefig(path_out + subject_id + '_comp_' + str(component+1)+ '.png')
-
+    plt.title('mean_' + mode  + ' , component ' + str(component+1))   
+    plt.savefig(path_out + 'mean_'+  mode + '_comp_' + str(component+1)+ '.png')
+    
+    
 #A_init = h5py.File('/nobackup/kocher1/bayrak/tmp/RcovS_468.h5', 'r')
 #A = np.array(A_init.get('cov'))
 
-## get mean of Aligned components over subjects
-#A_mean = np.mean(A, axis=0)
-#A_hem = A_mean[0:29696,:]
-##A_hem = A_mean[29696:29696+len(n),:]
-#A_hem = A_hem[:,0]
-#
-#data = np.zeros(len(vertices))
-#data[n] = A_hem
-#plotting.plot_surf_stat_map(vertices, triangles, stat_map=data, cmap='jet', azim=0)
 
-#del A_hem
-#A_hem = A[29696:29696+len(n)]
-#A_hem = A[0:29696]
-
-#data = np.zeros(len(vertices))
-#import h5py
-#A = h5py.File('/nobackup/kocher1/bayrak/embed_out/LH_node_average.h5', 'r')
-#L = A.get('sum')
-#data[n] = L[20015]
-#data[n] = L[:][10015]
-#plot_surf_stat_map(vertices, triangles, stat_map=data)
-#from mayavi import mlab
-#mlab.triangular_mesh(vertices[:, 0], vertices[:, 1], vertices[:, 2], triangles, scalars=data)
-#import numpy as np
-#b = np.loadtxt('/nobackup/kocher1/bayrak/embed_out/daniel_embedding.csv')
-#def vizData(a, n, num):
-#    data = np.zeros(len(vertices))
-#    data[n] = a[n,num]
-#    mlab.triangular_mesh(vertices[:, 0], vertices[:, 1], vertices[:, 2], triangles, scalars=data)    
-#vizData(a,n,0)

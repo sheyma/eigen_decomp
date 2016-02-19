31d2137f4543eb1a65f2766d6e8bbb59b4e99ff9
Şeyma Bayrak <sheymaba@gmail.com>
2016-02-19 01:28:57 +0100

    manipulate i/o filenames, do embedding, save results...

--- craddock_embed.py
+++ craddock_embed.py
@@ -9,29 +9,25 @@ import argparse
 ## parse command line arguments
 parser = argparse.ArgumentParser()
 # input prefix for subject path(s), e.g. /ptmp/sbayrak/hcp/*
-parser.add_argument("subject_dirs",nargs="+")
-# output prefix, e.g. /ptmp/sbayrak/corr_top10_out/top10_
-parser.add_argument('-o', '--outprfx', required=True)
-
+parser.add_argument("subject_dirs", nargs="+")
 args = parser.parse_args()
 
 filenames = np.array(args.subject_dirs)
 
 for i in range(0, len(filenames)):
     print "do loop %d/%d, %s" % (i+1, len(filenames), filenames[i])
-print "loading data - loop done"
-
-
-f = 'ptmp/sbayrak/franz_data/LI00000031/matrix.pkl'
-m = pickle.load((open(f)))['correlation']
-
-m += 1.0
-m /= 2.0
-
-embedding, result = embed.compute_diffusion_map(m , n_components=10)
-
-h = h5py.File(f, 'w')
-h.create_dataset('embedding', data = embedding)
-h.create_dataset('result', data = result)
-h.close()
+    f_in = os.path.join(filenames[i], 'matrix.pkl')
+    f_out = os.path.join(filenames[i], 'embedding.h5')		
+    m = pickle.load(open(f_in))['correlation']	
+    m += 1.0
+    m /= 2.0
+    embedding, results = embed.compute_diffusion_map(m , 
+  						    n_components=10)	
+    h = h5py.File(f_out, 'w')
+    h.create_dataset('embedding', data = embedding)
+    h.create_dataset('lambdas', data = results['lambdas'])
+    h.create_dataset('vectors', data = results['vectors'])
+    h.close()
+  
+print "loop done!"
 

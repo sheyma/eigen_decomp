1383a06f49cf8981e9361a0341bcb6947a5678b8
Şeyma Bayrak <sheymaba@gmail.com>
2016-02-20 23:55:44 +0100

    manipulate Cifti file, run wb_command, do smoothing, save out as hdf5

--- spatial_smooth.py
+++ spatial_smooth.py
@@ -1,7 +1,16 @@
-import sys, os
+import numpy as np
+import nibabel as nb
+import h5py
 import csv
-import pandas as pd
-import nipype.interfaces.freesurfer as fs
+from subprocess import call
+
+subject_list = []
+with open('/home/sheyma/tmp/subject_list.csv', 'rb') as f:
+    reader = csv.reader(f);
+    subject_list = list(reader);
+
+DATA = h5py.File('/home/sheyma/tmp/468_alignments.h5', 'r')
+mode = 'aligned'
 
 def choose_component(DATA, subject_id, mode, component = None):
     # choose all components of a given subject    
@@ -12,38 +21,39 @@ def choose_component(DATA, subject_id, mode, component = None):
         A = A[:, component]
     return A
 
-subject_list = []
-with open(path + 'subject_list.csv', 'rb') as f:
-    reader = csv.reader(f);
-    subject_list = list(reader);
-    
-DATA = h5py.File(path + '468_alignments.h5', 'r')
-mode = 'aligned'
+
+surf_lh = "/home/sheyma/devel/topography/data/Q1-Q6_R440.L.midthickness.32k_fs_LR.surf.gii"
+surf_rh = "/home/sheyma/devel/topography/data/Q1-Q6_R440.R.midthickness.32k_fs_LR.surf.gii"
+
+filename = '/home/sheyma/tmp/100307/rfMRI_REST1_LR_Atlas_hp2000_clean.dtseries.nii'
+A = nb.load(filename)
+
+
 component = 0
 DATA_all = []
-for subject_id in subject_list:
+for subject_id in subject_list[0]:
     subject_id = ''.join(subject_id)
+    print subject_id
     subject_component = choose_component(DATA, subject_id, mode, component)
-    DATA_all.append(subject_component)
-DATA_all = np.array(DATA_all)
+    tmp = np.array(subject_component)
+    print tmp.shape
+    A.data[0, 0:len(tmp)] = tmp
+    A.data[0, len(tmp):np.shape(A.data)[1]] = np.NaN
+    A.data[1:, :] = np.NaN
+    A.to_filename('/home/sheyma/tmp/' + subject_id + '_align.nii')
+    
+    B = '/home/sheyma/tmp/' + subject_id + '_align.nii' 
+    C = '/home/sheyma/tmp/' + subject_id + '_align_smooth.nii'
 
+    retcode = call(["wb_command", "-cifti-smoothing",  B, "2", "2", "COLUMN",
+                    C, "-left-surface", surf_lh, "-right-surface", surf_rh])  
 
-tmp_read_LH = pd.read_csv(path + 'data_32492_left_01.csv', header=None)
-tmp_read_LH = np.array(tmp_read_LH)
-
-name = '/a/documents/connectome/_all/100307/MNINonLinear/Results/rfMRI_REST1_LR/brainmask_fs.2.nii.gz'
-img = nb.load(name)
-
-A = nb.load('/a/documents/connectome/_all/100307/MNINonLinear/Results/rfMRI_REST1_LR/rfMRI_REST1_LR_Atlas_hp2000_clean.dtseries.nii')
-dir(A)
-A = nb.nifti2.load('/a/documents/connectome/_all/100307/MNINonLinear/Results/rfMRI_REST1_LR/rfMRI_REST1_LR_Atlas_hp2000_clean.dtseries.nii')
-dir(A)
-A.get_data().shape
-tmp = np.random.rand(1, 1, 1, 1, 1,  91282)
-A.dataobj[:] = tmp
-outcifti = nb.nifti2.Nifti2Image(tmp, A.get_affine(), header=A.get_header())
-outcifti.to_filename('tmp.nii')
-
-# fill uninteresting parts by NaN's
+    print retcode
+    
+    D = nb.load(C)
+    D = np.array(D.data[0, 0:len(tmp)])    
+    h = h5py.File('/home/sheyma/tmp/'+ subject_id+'align_smooth_59412.h5','w')
+    h.create_dataset('smooth', data=D)
+    h.close()
 
 

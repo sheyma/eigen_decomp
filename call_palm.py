3741fd23d271f2e00f1c393f20bcb48b47d06156
Şeyma Bayrak <sheymaba@gmail.com>
2016-03-01 17:03:14 +0100

    save left/right hem data separately in csv format

--- call_palm.py
+++ call_palm.py
@@ -1,8 +1,10 @@
 # run PALM...
 
-import sys, os
+import h5py
 import csv
 import pandas as pd
+import numpy as np
+import glob
 
 def choose_component(DATA, subject_id, mode, component = None):
     # choose all components of a given subject    
@@ -13,26 +15,62 @@ def choose_component(DATA, subject_id, mode, component = None):
         A = A[:, component]
     return A
 
-subject_list = []
-with open(path + 'subject_list.csv', 'rb') as f:
-    reader = csv.reader(f);
-    subject_list = list(reader);
-
-DATA = h5py.File(path + '468_embeddings.h5', 'r')
-mode = 'embedding'
-
-DATA = h5py.File(path + '468_alignments.h5', 'r')
-mode = 'aligned'
-component = 0
-DATA_all = []
-for subject_id in subject_list:
-    subject_id = ''.join(subject_id)
-    subject_component = choose_component(DATA, subject_id, mode, component)
-    DATA_all.append(subject_component)
-DATA_all = np.array(DATA_all)
+def get_surface(surface_data, hemisphere, surface_type):
+    """
+    surface_data = hdf5 formatted surface data
+    hemisphere = 'LH', 'RH', or 'full'
+    surface_type = 'midthickness', 'inflated', or 'very_inflated'
+    """
+    tmp = h5py.File(surface_data, 'r')
+    indices = np.array( tmp[hemisphere][surface_type]['indices'] )
+    vertices = np.array( tmp[hemisphere][surface_type]['vertices'] )    
+    triangles = np.array( tmp[hemisphere][surface_type]['triangles'])
+    
+    return indices, vertices, triangles
 
 
-surface_data = path + 'data_surface.h5'
+def get_csv(DATA, subject_list, mode, components, n, n_LH, n_RH, vertices,
+            vertices_LH, vertices_RH, path_out):
+                
+    for component in components:
+        print "loop for component ", component + 1
+    
+        DATA_all = []
+        
+        for subject_id in subject_list:
+            subject_id = ''.join(subject_id)
+            subject_component = choose_component(DATA, subject_id, mode, component)
+            DATA_all.append(subject_component)
+        
+        DATA_all = np.array(DATA_all)
+        DATA_LH = DATA_all[:, 0:len(n_LH)]
+        DATA_RH = DATA_all[:, len(n_LH):len(n_RH)+len(n_LH)]
+        
+        data_all = np.zeros((len(subject_list), len(vertices)))
+        data_LH = np.zeros((len(subject_list), len(vertices_LH)))
+        data_RH = np.zeros((len(subject_list), len(vertices_RH)))
+        
+        data_all[:, n] = DATA_all
+        data_LH[:, n_LH] = DATA_LH
+        data_RH[:, n_RH] = DATA_RH
+        
+        if component != 9:
+            name_LH = path_out + 'Sdata_32492_left_0' + str(component+1) + '.csv'    
+            name_RH = path_out + 'Sdata_32492_right_0' + str(component+1) + '.csv'
+        else:
+            name_LH = path_out + 'Sdata_32492_left_' + str(component+1) + '.csv'    
+            name_RH = path_out + 'Sdata_32492_right_' + str(component+1) + '.csv'
+            
+        tmp_LH = pd.DataFrame(data_LH)
+        tmp_LH.to_csv(name_LH, header=False, index=False)
+        
+        tmp_RH = pd.DataFrame(data_RH)
+        tmp_RH.to_csv(name_RH, header=False, index=False)
+
+    return
+
+
+surface_data = 'data/data_surface.h5'
 surface_type = 'midthickness'
 
 hemisphere = 'full'
@@ -45,80 +83,36 @@ hemisphere = 'RH'
 n_RH, vertices_RH, triangles_RH = get_surface(surface_data, 
                                               hemisphere, surface_type)
 
+path_in = '/nobackup/kocher1/bayrak/tmp/'
+path_out = '/nobackup/kocher1/bayrak/palm_data/'
 
-data_full = np.zeros((len(subject_list), len(vertices)))
-data_full[:, n] = DATA_all
+DATA = h5py.File(path_in + '468_smoothing_new.h5', 'r')
+mode = 'smooth'
+components = np.arange(0,10,1)
 
-
-DATA_all_LH = DATA_all[:, 0:len(n_LH)]
-data_LH = np.zeros((len(subject_list), len(vertices_LH)))
-data_LH[:, n_LH] = DATA_all_LH
-
-DATA_all_RH = DATA_all[:, len(n_LH):len(n_RH)+len(n_LH)]
-data_RH = np.zeros((len(subject_list), len(vertices_RH)))
-data_RH[:, n_RH] = DATA_all_RH
-
-tmp_full = pd.DataFrame(data_full)
-tmp_full.to_csv(path + 'data_64984_full_01.csv', header=False, index=False)
-
-tmp_LH = pd.DataFrame(data_LH)
-tmp_LH.to_csv(path + 'data_32492_left_01.csv', header=False, index=False)
-
-tmp_RH = pd.DataFrame(data_RH)
-tmp_RH.to_csv(path + 'data_32492_right_01.csv', header=False, index=False)
-
-
-tmp_read_full = pd.read_csv(path + 'data_64984_full_01.csv', header=None)
-tmp_read_full = np.array(tmp_read_full)
-
-tmp_read_LH = pd.read_csv(path + 'data_32492_left_01.csv', header=None)
-tmp_read_LH = np.array(tmp_read_LH)
-
-tmp_read_RH = pd.read_csv(path + 'data_32492_right_01.csv', header=None)
-tmp_read_RH = np.array(tmp_read_RH)
-
-
-plotting.plot_surf_stat_map(vertices, triangles, 
-                            stat_map=tmp_read_full[447, :], 
-                            cmap='jet', azim=180)
-
-plotting.plot_surf_stat_map(vertices_LH, triangles_LH, 
-                            stat_map=tmp_read_LH[447, :],
-                            cmap='jet', azim=180)
-
-plotting.plot_surf_stat_map(vertices_RH, triangles_RH, 
-                            stat_map=tmp_read_RH[447, :],
-                            cmap='jet', azim=0)
-
-
-sys.path.append(os.path.expanduser('/home/raid/bayrak/src/PALM'))
-os.system('./palm')
-
-# load a certain component for each subject and save as .csv
-
-
-
-
-
-./palm -i ~/tmp/data_lh.csv -s ~/devel/topography/data/lh.pial -d ~/tmp/design_matrix.csv -t test.con -n 1000 -o myresults/second_run  -zstat -fdr -logp
-
-path = '/home/raid/bayrak/tmp/'
-
-# read-in subject_list
 subject_list = []
-with open(path + 'subject_list.csv', 'rb') as f:
+with open('data/subject_list.csv', 'rb') as f:
     reader = csv.reader(f);
     subject_list = list(reader);
 
+#get_csv(DATA, subject_list, mode, components, n, n_LH, n_RH, vertices,
+#            vertices_LH, vertices_RH, path_out)
 
-# save out group level results...
-tmp_list = []
 
-# plot a component over all subjects
-components = np.arange(0, 10, 1)
-for component in components:
-    #tmp = get_mean(DATA, subject_list, mode, component)
-   
-    tmp = get_cov(DATA, DATA_new, subject_list, mode, mode_new, 
-                      comp=component, comp_new=None)    
-    tmp_list.append(tmp)
+#left_list = glob.glob(path_out + '*left*csv')
+#right_list = glob.glob(path_out + '*right*csv')
+
+#tmp_read_LH = pd.read_csv(name_LH, header=None)
+#tmp_read_LH = np.array(tmp_read_LH)
+
+tmp_name = '/nobackup/kocher1/bayrak/palm_data/Sdata_32492_left_05.csv' 
+tmp_surf = '/nobackup/kocher1/bayrak/palm_data/lh.pial'
+
+#sys.path.append(os.path.expanduser('/home/raid/bayrak/src/PALM'))
+#os.system('./palm')
+
+# load a certain component for each subject and save as .csv
+
+#./palm -i ~/tmp/data_lh.csv -s ~/devel/topography/data/lh.pial -d ~/tmp/design_matrix.csv -t test.con -n 1000 -o myresults/second_run  -zstat -fdr -logp
+#
+#path = '/home/raid/bayrak/tmp/'
